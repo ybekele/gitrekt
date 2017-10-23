cbbaa8f245ea06af1ed55214cb2c01fc515654aa
yonael <yonael@ualberta.ca>
2017-10-22 21:41:07 -0600

    Updating Testing

---
+++ HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTrackActivityTest.java
@@ -0,0 +1,32 @@
+package com.example.habitrack;
+/**
+ * Created by yonaelbekele on 2017-10-22.
+ */
+
+public class HabitTrackActivityTest {
+    /*
+    private Solo solo;
+
+    public HabiTrackActivityTest() {
+        super(ca.ualberta.cs.habitrack.HabiTrackActivity.class);
+    }
+
+    public void setUp() throws Exception{
+        solo = new Solo.getInstrumentation(), getActivity());
+    }
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
+    }
+
+        @Override
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+
+    // Test habit
+
+    // Test create habit
+
+    // Test create habit event
+
+     */
+}
