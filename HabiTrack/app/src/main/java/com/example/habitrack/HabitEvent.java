4ffcf0ad67ae480e187a572635d983524bbf26b6
sshussai <sshussai@ualberta.ca>
2017-10-23 14:53:24 -0600

    Add basic functions to HabitEvent Entity

--- HabiTrack/app/src/main/java/com/example/habitrack/HabitEvent.java
+++ HabiTrack/app/src/main/java/com/example/habitrack/HabitEvent.java
@@ -8,7 +8,23 @@ package com.example.habitrack;
 public class HabitEvent {
     String word = "hello";
 
+    private String title;
+    private boolean status;
+    private int eventsDone;
+    private int totalEventCount;
 
+    public String getTitle(){
+        return this.title;
+    }
+
+    public float getProgress(){
+        return 100*(eventsDone/totalEventCount);
+    }
+
+    public boolean getStatus(){
+        return false;
+    }
+    
     public String get_my_Word() {
         return word;
     }
