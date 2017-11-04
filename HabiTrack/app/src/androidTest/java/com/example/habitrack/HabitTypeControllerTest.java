bfb73656e23fefdddc7195eaeebf37975ca1fe5a
sshussai <sshussai@ualberta.ca>
2017-11-04 15:21:00 -0600

    modify testcreateht(), and add testdeleteht() functions

--- HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTypeControllerTest.java
+++ HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTypeControllerTest.java
@@ -19,6 +19,7 @@ public class HabitTypeControllerTest extends TestCase {
 
     // Test Habit type
     HabitType ht;
+    HabitType newht;
 
     // Function that runs before any tests
     protected void setUp(){
@@ -33,11 +34,20 @@ public class HabitTypeControllerTest extends TestCase {
     public void testCreateHabit(){
         HabitTypeController htc = new HabitTypeController();
         htc.createNewHabitType(title, reason, startDate, schedule);
-        ht = HabitTypeStateManager.getHTStateManager().getHabitType(1);
-        assertEquals(title, ht.getTitle());
-        assertEquals(reason, ht.getReason());
-        assertTrue(startDate.equals(ht.getStartDate()));
-        assertTrue(schedule.equals(ht.getSchedule()));
+        this.ht = HabitTypeStateManager.getHTStateManager().getHabitType(1);
+        assertEquals(title, this.ht.getTitle());
+        assertEquals(reason, this.ht.getReason());
+        assertTrue(startDate.equals(this.ht.getStartDate()));
+        assertTrue(schedule.equals(this.ht.getSchedule()));
+
+    }
+
+    public void testDeleteHabit(){
+        HabitTypeStateManager.getHTStateManager().removeHabitType(1);
+        newht = HabitTypeStateManager.getHTStateManager().getHabitType(1);
+        Integer test = -1;
+        Integer retID = newht.getID();
+        assertEquals(test, retID);
     }
 
     // Fucntion that runs after all tests
