65295df3a1fa16d379c3d1c767205d27404422d8
yonael <yonael@ualberta.ca>
2017-11-09 21:48:41 -0700

    Working tests + UI

--- HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTrackActivityTest.java
+++ HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTrackActivityTest.java
@@ -1,6 +1,5 @@
 package com.example.habitrack;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.robotium.solo.Solo;
@@ -11,57 +10,51 @@ import com.robotium.solo.Solo;
 
 public class HabitTrackActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
     private Solo solo;
-    public HabitTrackActivityTest(Class<MainActivity> activityClass) {
-        super(activityClass);
+
+
+    public HabitTrackActivityTest() {
+        super(MainActivity.class);
     }
 
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+/*
     public void testStart() throws Exception {
         Activity activity = getActivity();
-    }
-    @Override
-    public void tearDown() throws Exception {
-        solo.finishOpenedActivities();
+    }*/
 
-        // Test habit
 
-        // Test create habit
 
-        // Test create habit event
-    }
+    public void testInvalidType() throws Exception {
+        solo.assertCurrentActivity("Wrong Activity, not main activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.button));
+        //solo.clickOnButton(solo.getString(R.string.button));
 
-    public void testActivities() throws Exception {
-        solo.clickOnButton(R.id.button);
 
         solo.waitForActivity(NewHabitTypeActivity.class);
         solo.assertCurrentActivity("Failed to Switch to NewHabitTypeActivity",NewHabitTypeActivity.class);
-
-
-
+        solo.clickOnView(solo.getView(R.id.button6));
+        solo.waitForText("Invalid Creation");
+        solo.waitForActivity(MainActivity.class);
+        solo.assertCurrentActivity("Failed to Switch back to MainActivity", MainActivity.class);
     }
 
-
-/*
-    public HabiTrackActivityTest() {
-        super(ca.ualberta.cs.habitrack.HabiTrackActivity.class);
+    public void testValidType() throws Exception {
+        solo.assertCurrentActivity("Wrong Activity, not main activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.button));
+        solo.waitForActivity(NewHabitTypeActivity.class);
+        solo.assertCurrentActivity("Failed to Switch to NewHabitTypeActivity",NewHabitTypeActivity.class);
+        solo.clickOnView(solo.getView(R.id.button6));
+        solo.waitForActivity(MainActivity.class);
+        solo.assertCurrentActivity("Failed to Switch back to MainActivity", MainActivity.class);
     }
 
-    public void setUp() throws Exception{
-        solo = new Solo.getInstrumentation(), getActivity();
-    }
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
-    }
-
-        @Override
+    @Override
     public void tearDown() throws Exception {
-            solo.finishOpenedActivities();
+        solo.finishOpenedActivities();
+    }}
 
-            // Test habit
 
-            // Test create habit
 
-            // Test create habit event
-        }
-        */
 
-}
