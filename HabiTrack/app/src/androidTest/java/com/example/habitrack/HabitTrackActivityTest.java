60662baea094034df6e36f2074d0d97fe0156ef1
yonael <yonael@ualberta.ca>
2017-11-22 16:34:38 -0700

    Robotium Testing

--- HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTrackActivityTest.java
+++ HabiTrack/app/src/androidTest/java/com/example/habitrack/HabitTrackActivityTest.java
@@ -42,6 +42,10 @@ public class HabitTrackActivityTest extends ActivityInstrumentationTestCase2<Mai
         solo.assertCurrentActivity("Wrong activity, not main activity", MainActivity.class);
     }
 
+    /**
+     * Tests handling of invalid Habit Type
+     * @throws Exception
+     */
     public void testInvalidType() throws Exception {
         /*
         solo.clickOnView(solo.getView(R.id.editText));
@@ -73,6 +77,8 @@ public class HabitTrackActivityTest extends ActivityInstrumentationTestCase2<Mai
         */
         solo.assertCurrentActivity("Wrong Activity, not main activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.createHabitButton));
+
+        // Entering The NewHabitTypeActivity
         solo.waitForActivity(NewHabitTypeActivity.class);
         solo.assertCurrentActivity("Failed to Switch to NewHabitTypeActivity",NewHabitTypeActivity.class);
         solo.enterText((EditText) solo.getView(R.id.editText3), "test title");
@@ -91,16 +97,53 @@ public class HabitTrackActivityTest extends ActivityInstrumentationTestCase2<Mai
         assert(!solo.waitForText("Invalid Creation")); /* Asserts that the User has created a new Habit Type */
         solo.waitForActivity(MainActivity.class);
         solo.assertCurrentActivity("Failed to Switch back to MainActivity", MainActivity.class);
-        //solo.clickInList(1);
-        //solo.waitForActivity(NewHabitEventActivity.class);
-        //solo.assertCurrentActivity("Failed to Go to new Habit Event", NewHabitEventActivity.class);
-        //solo.enterText((EditText) solo.getView(R.id.heCommentBox), "test event");
-        //solo.clickOnView(solo.getView(R.id.addEventButton));
+        solo.clickInList(1);
+
+        // Entering the NewHabitEvent
+        solo.waitForActivity(NewHabitEventActivity.class);
+        solo.assertCurrentActivity("Failed to Go to new Habit Event", NewHabitEventActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.heCommentBox), "test event");
+        solo.clickOnView(solo.getView(R.id.addEventButton));
+        solo.waitForActivity(MainActivity.class);
+        solo.assertCurrentActivity("Failed to Switch back to MainActivity", MainActivity.class);
     }
 
+    public void testHistory() throws Exception {
+        solo.assertCurrentActivity("Wrong Activity, not main activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.historyButton));
+        solo.waitForActivity(HistoryActivity.class);
+        solo.assertCurrentActivity("Wrong Activity, not habit history", HabitHistory.class);
+        solo.clickInList(1);
 
+    }
 
+    /**
+     * Tests the editing of the schedule in AllHabitType
+     * @throws Exception
+     */
+    public void testSchedule() throws Exception {
+        solo.assertCurrentActivity("Wrong Activity, not main activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.allButton));
+        solo.clickInList(1);
+        solo.waitForActivity(HabitTypeDetailsActivity.class);
+        solo.assertCurrentActivity("Wrong Activity, not habit type details", HabitTypeDetailsActivity.class);
+        solo.clickOnView(solo.getView(R.id.satCheckBox));
+        solo.clickOnView(solo.getView(R.id.monCheckBox));
+        solo.clickOnView(solo.getView(R.id.tuesCheckBox));
+        solo.clickOnView(solo.getView(R.id.wedCheckBox));
+        solo.clickOnView(solo.getView(R.id.editSchedule));
+    }
 
+    public void testRemoveType() throws Exception {
+        solo.assertCurrentActivity("Wrong Activity, not main activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.allButton));
+        solo.clickInList(1);
+        solo.waitForActivity(HabitTypeDetailsActivity.class);
+        solo.assertCurrentActivity("Wrong Activity, not habit type details", HabitTypeDetailsActivity.class);
+        solo.clickOnView(solo.getView(R.id.deleteButton));
+        solo.waitForActivity(HabitTypeDetailsActivity.class);
+        solo.assertCurrentActivity("Wrong Activity, not habit type details", HabitTypeDetailsActivity.class);
+    }
 
 
 
