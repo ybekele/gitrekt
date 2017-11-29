f3a5391bb4460466e6090a4f378f789f8cdf3813
yonael <yonael@ualberta.ca>
2017-11-29 11:17:24 -0700

    Manifest fixes

---
+++ HabiTrack/app/src/main/java/com/example/habitrack/FollowersActivity.java
@@ -0,0 +1,10 @@
+package com.example.habitrack;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yonaelbekele on 2017-11-28.
+ */
+
+public class FollowersActivity extends AppCompatActivity {
+}
---
+++ HabiTrack/app/src/main/java/com/example/habitrack/SocialActivity.java
@@ -0,0 +1,17 @@
+package com.example.habitrack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yonaelbekele on 2017-11-28.
+ */
+
+public class SocialActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_activity);
+}
+}
+
