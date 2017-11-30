8fbcb71eeffce8d452667fb8a01fbbaf35de42a9
sshussai <sshussai@ualberta.ca>
2017-11-29 23:48:38 -0700

    add ht metadata class to store ht info for offline functionality

---
+++ HabiTrack/app/src/main/java/com/example/habitrack/HabitTypeMetadata.java
@@ -0,0 +1,67 @@
+package com.example.habitrack;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sshussai on 11/29/17.
+ */
+
+public class HabitTypeMetadata {
+
+    private Integer localID;
+    private String esID;
+    private String title;
+    private Boolean canBeScheduled;
+    private ArrayList<Integer> schedule;
+
+
+    public HabitTypeMetadata(Integer localID, String esID) {
+        this.localID = localID;
+        this.esID = esID;
+    }
+
+    public Integer getLocalID() {
+        return localID;
+    }
+
+    public void setLocalID(Integer localID) {
+        this.localID = localID;
+    }
+
+    public String getEsID() {
+        return esID;
+    }
+
+    public void setEsID(String esID) {
+        this.esID = esID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCanBeScheduled() {
+        return canBeScheduled;
+    }
+
+    public void setCanBeScheduled(Boolean canBeScheduled) {
+        this.canBeScheduled = canBeScheduled;
+    }
+
+    public ArrayList<Integer> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(ArrayList<Integer> schedule) {
+        this.schedule = schedule;
+    }
+
+    @Override
+    public String toString() {
+        return esID + "|" + localID + "|" + title;
+    }
+}
