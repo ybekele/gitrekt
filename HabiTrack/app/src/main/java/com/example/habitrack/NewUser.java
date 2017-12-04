27df300fb3cb9b1a3427ad3498c0d2bfcb8caa15
Harjot Rai <hrai@ualberta.ca>
2017-12-04 16:45:59 -0700

    Changes to some login stuff

--- HabiTrack/app/src/main/java/com/example/habitrack/NewUser.java
+++ HabiTrack/app/src/main/java/com/example/habitrack/NewUser.java
@@ -37,16 +37,16 @@ public class NewUser {
         return userID;
     }
 
-    public String findId(String userName) {
-        return userID;
-    }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
     public void setUserID(String userID) {
         this.userID = userID;
+    }
+
+    public String getUserID(String userName) {
+        return userID;
     }
 
     public void addUsersFollowed(NewUser follower) {
